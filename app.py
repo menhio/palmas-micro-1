ba7d05ffff1e572bfb9d3a913a68c14ab56729e0
Manuel Salazar <m1_20@hotmail.com>
2022-04-06 17:34:50 -0600

    setup

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    print(os.environ.get('DR_DATABASE_USER'))
+    return {
+        'status': 'ok',
+    }, 200
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
